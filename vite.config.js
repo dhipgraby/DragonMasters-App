930cc6354e8e2549912762eac8666bd34ce95ad9
thecil <53628989+thecil@users.noreply.github.com>
2022-09-01 12:48:58 -0400

    migration sveltekit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	server: {
+		// host: '0.0.0.0',
+		port: 3000,
+		// hmr: {
+		//   host: 'localhost',
+		//   protocol: 'wss',
+		//   port: 3001
+		// },
+		fs: {
+			allow: ['static'],
+		},
+	},
+};
+
+export default config;
