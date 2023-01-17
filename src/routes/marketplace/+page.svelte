a3109caad15d4eb4a96c7c7ea5947855b53051dd
kennow <kenneth.zambrano4@gmail.com>
2023-01-17 17:49:02 +0100

    No offers found for all offers

--- src/routes/marketplace/+page.svelte
+++ src/routes/marketplace/+page.svelte
@@ -9,7 +9,7 @@
 		dragonsForRent,
 		eggsForRent
 	} from '$lib/storage/marketplace';
-	import { TokenType, OfferType } from '$lib/contracts/LoanBook';	
+	import { TokenType, OfferType } from '$lib/contracts/LoanBook';
 	import { get_unique_tokenid } from '$lib/helpers/utils';
 	import TokenButtons from '$lib/component/marketplace/TokenButtons.svelte';
 	import OfferTypeBtn from '$lib/component/marketplace/OfferTypeBtn.svelte';
@@ -28,31 +28,29 @@
 	//RENT OFFERS
 	$: dragons_for_rent = $dragonsForRent;
 	$: eggs_for_rent = $eggsForRent;
-	let allOffers; 
-
-	const allAssets = () => {
-		const offers =  {
-			dragons: get_unique_tokenid(dragons_for_sale, dragons_for_rent),
-			eggs: get_unique_tokenid(eggs_for_sale, eggs_for_rent)			
-		};
-		return {
-			dragons: orderByOffer(offers.dragons,OfferType.ForSale),
-			eggs: orderByOffer(offers.eggs,OfferType.ForSale),
-		}
+	let allOffers = {
+		eggs: [],
+		dragons: []
 	};
 
-	const changeToken = (_tokenType) => {		
+	const allAssets = () => {
+		return {
+			dragons: get_unique_tokenid(dragons_for_sale, dragons_for_rent),
+			eggs: get_unique_tokenid(eggs_for_sale, eggs_for_rent)
+		};
+	};
+
+	const changeToken = (_tokenType) => {
 		show = _tokenType;
 	};
 
 	const setOfferType = (newType) => {
-		_offerType = newType
-		console.log(_offerType);
-	}
+		_offerType = newType;
+	};
 
 	onMount(async () => {
-		await LoadInterface(0, perpage);				
-		allOffers = allAssets()		
+		await LoadInterface(0, perpage);
+		allOffers = allAssets();
 		console.log(allOffers);
 	});
 </script>
@@ -114,34 +112,42 @@
 		{/if}
 	{/if}
 	{#if _offerType == OfferType.ForSaleOrRent}
-
-	{#if show == TokenType.Egg}
-		<MarketGrid
-			{_offerType}
-			assets={allOffers.eggs}
-			contract={contractsData}
-			loadPage={LoadInterface}
-			{perpage}
-			_tokenType={TokenType.Egg}
-		/>
+		{#if show == TokenType.Egg}
+			{#if allOffers.eggs.length >= 1}
+				<MarketGrid
+					{_offerType}
+					assets={allOffers.eggs}
+					contract={contractsData}
+					loadPage={LoadInterface}
+					{perpage}
+					_tokenType={TokenType.Egg}
+				/>
+			{:else}
+				<h2>No offers found...</h2>
+			{/if}
+		{/if}
+		{#if show == TokenType.Dragon}
+			{#if allOffers.dragons.length >= 1}
+				<MarketGrid
+					{_offerType}
+					assets={allOffers.dragons}
+					contract={contractsData}
+					loadPage={LoadInterface}
+					{perpage}
+					_tokenType={TokenType.Dragon}
+				/>
+			{:else}
+				<h2>No offers found...</h2>
+			{/if}
+		{/if}
 	{/if}
-	{#if show == TokenType.Dragon}
-		<MarketGrid
-			{_offerType}
-			assets={allOffers.dragons}
-			contract={contractsData}
-			loadPage={LoadInterface}
-			{perpage}
-			_tokenType={TokenType.Dragon}
-		/>
-	{/if}
-{/if}
 </MainContainer>
 
 <style>
-
+	h2 {
+		margin-top: 20px;	
+	}
 	h1 {
 		margin-bottom: 25px;
 	}
-
 </style>
