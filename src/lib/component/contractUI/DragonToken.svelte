28fc19c38c3e6d9a75f95e507ad8d483cc4dfb1f
kennow <kenneth.zambrano4@gmail.com>
2023-01-25 12:13:16 +0100

    Fix Contract events on DragonToken UI

--- src/lib/component/contractUI/DragonToken.svelte
+++ src/lib/component/contractUI/DragonToken.svelte
@@ -2,68 +2,67 @@
 	import { onMount } from 'svelte';
 	import Pagination from '$lib/component/pagination/UIpagination.svelte';
 	import { setAlert } from '$lib/storage/alerts';
+	import { DragonContract } from '$lib/contracts/DragonToken';
 	import { initEventListener } from '$lib/contracts/events';
 
-	export let contract;
-
+	let contract;
 	let relatedA;
 	let relatedB;
 
-	let OwnerAddress
-	let startIndex = 0
-	let endIndex = 9
-	let singleId
-	let skillId
-	let tokenIds = ""
-	let tokenIdsA = ""
-	let tokenIdsB = ""
+	let OwnerAddress;
+	let startIndex = 0;
+	let endIndex = 9;
+	let singleId;
+	let skillId;
+	let tokenIds = '';
+	let tokenIdsA = '';
+	let tokenIdsB = '';
 
-	onMount(async() => {
+	onMount(async () => {
+		contract = await new DragonContract();
+		let contractEvents = await contract.contract?.DragonToken.events;
 
-		let contractEvents = await contract.contract.DragonToken.events;		
-	
 		const updater = (event) => {
-			const newEggs = []
+			const newEggs = [];
 			const returnValues = event.returnValues;
 			if (returnValues.eggIds.length > 0) {
 				const eggIds = event.returnValues.eggIds;
 				const provenance = event.returnValues.provenance;
-				if(eggIds[0] !== undefined)  {
-					newEggs.push(getNewEgg(eggIds[0],provenance[0]))
-					setAlert('One new egg laid! Id: '+eggIds[0], 'success')
+				if (eggIds[0] !== undefined) {
+					newEggs.push(getNewEgg(eggIds[0], provenance[0]));
+					setAlert('One new egg laid! Id: ' + eggIds[0], 'success');
 				}
-				if(eggIds[1] !== undefined)  {
-					newEggs.push(getNewEgg(eggIds[1],provenance[1]))
-					setAlert('A second egg laid! Id: '+eggIds[1], 'success')
+				if (eggIds[1] !== undefined) {
+					newEggs.push(getNewEgg(eggIds[1], provenance[1]));
+					setAlert('A second egg laid! Id: ' + eggIds[1], 'success');
 				}
-			} 
-			else {
-				setAlert('Unlucky, no eggs laid!', 'success')
+			} else {
+				setAlert('Unlucky, no eggs laid!', 'success');
 			}
-			console.log(newEggs)
+			console.log(newEggs);
 		};
 
 		await initEventListener(contractEvents, updater, 'DragonToken');
 		await getEvents();
-	})
+	});
 
 	async function getEvents() {
-        const events = await contract.contract.DragonToken.getPastEvents('EggsLaid', {
-            fromBlock: 0,
-            toBlock: 'latest'
-        });
-        console.log(events);
-    }
+		const events = await contract.contract.DragonToken.getPastEvents('EggsLaid', {
+			fromBlock: 0,
+			toBlock: 'latest'
+		});
+		console.log(events);
+	}
 
 	function getNewEgg(eggId, provenance) {
-        return {
-            eggId: eggId,
-            dadId: provenance.dadId,
-            generation: provenance.generation,
-            mumId: provenance.mumId,
-            species: provenance.species
-        };
-    }
+		return {
+			eggId: eggId,
+			dadId: provenance.dadId,
+			generation: provenance.generation,
+			mumId: provenance.mumId,
+			species: provenance.species
+		};
+	}
 
 	function changeIndex(indexType, value) {
 		switch (indexType) {
@@ -115,7 +114,7 @@
 	async function breed() {
 		if (tokenIdsA.length == 0 && tokenIdsB.length == 0)
 			setAlert('Specify at least one pair of dragons for breeding', 'warning');
-			
+
 		let mateA_Ids = [];
 		if (tokenIdsA.length > 0) {
 			mateA_Ids = tokenIdsA.split(',').map(Number);
@@ -128,7 +127,7 @@
 			setAlert('Specify breeding pairs, ie. equal number of mate As & Bs', 'warning');
 		else {
 			contract.breed(tokenIdsA, tokenIdsB, true);
-		}	
+		}
 	}
 
 	async function pause() {
@@ -138,24 +137,29 @@
 	async function unpause() {
 		contract.unpause(true);
 	}
-
 </script>
 
 <h1>DragonToken</h1>
 
-<p class="bold">Contract Status: UNPAUSED</p>		
+<p class="bold">Contract Status: UNPAUSED</p>
 <button class="btn btn-dark text-light bold mb-3 mt-3" on:click={() => pause()}>Pause</button>
 <button class="btn btn-dark text-light bold mb-3 mt-3" on:click={() => unpause()}>Unpause</button>
-<button class="btn btn-info text-light bold mb-3 mt-3" on:click={() => contract.totalSupply()}>Total Supply</button>
+<button class="btn btn-info text-light bold mb-3 mt-3" on:click={() => contract.totalSupply()}
+	>Total Supply</button
+>
 
 <div class="row">
 	<div class="col-sm-12 col-md-12 col-xl-4">
-
 		<div class="grid" align="left">
 			<h2>Get Dragon Ids</h2>
-			<p class="bold">Of owner</p>		
+			<p class="bold">Of owner</p>
 			<div class="mb-3">
-				<input type="text" bind:value={OwnerAddress} class="form-control mb-3" placeholder="address"/>
+				<input
+					type="text"
+					bind:value={OwnerAddress}
+					class="form-control mb-3"
+					placeholder="address"
+				/>
 			</div>
 			<p class="bold">Paging: start & end indexes</p>
 			<div class="mb-3">
@@ -166,100 +170,106 @@
 
 		<div class="grid" align="left">
 			<h2>Get Dragon</h2>
-			<p class="bold">Token Id</p>		
+			<p class="bold">Token Id</p>
 			<div class="mb-3">
-				<input type="text" bind:value={singleId} class="form-control" placeholder="0"/>
+				<input type="text" bind:value={singleId} class="form-control" placeholder="0" />
 			</div>
 			<button class="btn btn-dark" on:click={() => getDragon()}>GET</button>
 		</div>
 
 		<div class="grid" align="left">
 			<h2>Get Skills</h2>
-			<p class="bold">Token Id</p>		
+			<p class="bold">Token Id</p>
 			<div class="mb-3">
-				<input type="text" bind:value={singleId} class="form-control" placeholder="0"/>
+				<input type="text" bind:value={singleId} class="form-control" placeholder="0" />
 			</div>
 			<button class="btn btn-dark" on:click={() => getSkills()}>GET</button>
 		</div>
 
 		<div class="grid" align="left">
 			<h2>Get Skill Level</h2>
-			<p class="bold">Token Id</p>		
+			<p class="bold">Token Id</p>
 			<div class="mb-3">
-				<input type="text" bind:value={singleId} class="form-control" placeholder="0"/>
+				<input type="text" bind:value={singleId} class="form-control" placeholder="0" />
 			</div>
-			<p class="bold">Skill Id (0->7)</p>		
+			<p class="bold">Skill Id (0->7)</p>
 			<div class="mb-3">
-				<input type="text" bind:value={skillId} class="form-control" placeholder="0"/>
+				<input type="text" bind:value={skillId} class="form-control" placeholder="0" />
 			</div>
 			<button class="btn btn-dark" on:click={() => getSkillLevel()}>GET</button>
 		</div>
 
 		<div class="grid" align="left">
 			<h2>Get Skills with their levels</h2>
-			<p class="bold">Token Id</p>		
+			<p class="bold">Token Id</p>
 			<div class="mb-3">
-				<input type="text" bind:value={singleId} class="form-control" placeholder="0"/>
+				<input type="text" bind:value={singleId} class="form-control" placeholder="0" />
 			</div>
 			<button class="btn btn-dark" on:click={() => getSkillsWithLevels()}>GET</button>
 		</div>
 	</div>
 
 	<div class="col-sm-12 col-md-12 col-xl-4">
-
 		<div class="grid" align="left">
 			<h2>Check Maturity</h2>
-			<p class="bold">Token Id</p>		
+			<p class="bold">Token Id</p>
 			<div class="mb-3">
-				<input type="text" bind:value={singleId} class="form-control" placeholder="0"/>
+				<input type="text" bind:value={singleId} class="form-control" placeholder="0" />
 			</div>
 			<button class="btn btn-dark" on:click={() => checkMaturity()}>CHECK</button>
 		</div>
 
 		<div class="grid" align="left">
 			<h2>Check Energy</h2>
-			<p class="bold">Token Id</p>		
+			<p class="bold">Token Id</p>
 			<div class="mb-3">
-				<input type="text" bind:value={singleId} class="form-control" placeholder="0"/>
+				<input type="text" bind:value={singleId} class="form-control" placeholder="0" />
 			</div>
 			<button class="btn btn-dark" on:click={() => checkEnergy()}>CHECK</button>
 		</div>
 
 		<div class="grid" align="left">
 			<h2>Get Relationship (A to B)</h2>
-			<p class="bold">A: Token Id</p>		
+			<p class="bold">A: Token Id</p>
 			<div class="mb-3">
-				<input type="text" bind:value={relatedA} class="form-control mb-3" placeholder="0"/>
-				<p class="bold">B: Token Id</p>		
+				<input type="text" bind:value={relatedA} class="form-control mb-3" placeholder="0" />
+				<p class="bold">B: Token Id</p>
 				<input type="text" bind:value={relatedB} class="form-control" placeholder="1" />
 			</div>
 			<button class="btn btn-dark" on:click={() => getRelationship()}>GET</button>
 		</div>
-
 	</div>
 
 	<div class="col-sm-12 col-md-12 col-xl-4">
-
 		<div class="grid" align="left">
 			<h2>Raise Maturity</h2>
-			<p class="bold">Token Ids</p>		
+			<p class="bold">Token Ids</p>
 			<div class="mb-3">
-				<input type="text" bind:value={tokenIds} class="form-control" placeholder="0, 1, ..."/>
+				<input type="text" bind:value={tokenIds} class="form-control" placeholder="0, 1, ..." />
 			</div>
 			<button class="btn btn-dark" on:click={() => raiseMaturity()}>Raise</button>
 		</div>
 
 		<div class="grid" align="left">
 			<h2>Breed (A with B)</h2>
-			<p class="bold">Mate A: Token Ids</p>		
+			<p class="bold">Mate A: Token Ids</p>
 			<div class="mb-3">
-				<input type="text" bind:value={tokenIdsA} class="form-control mb-3" placeholder="0, 1, ..."/>
-				<p class="bold">Mate B: Token Ids</p>		
+				<input
+					type="text"
+					bind:value={tokenIdsA}
+					class="form-control mb-3"
+					placeholder="0, 1, ..."
+				/>
+				<p class="bold">Mate B: Token Ids</p>
 				<input type="text" bind:value={tokenIdsB} class="form-control" placeholder="3, 5, ..." />
 			</div>
-			<p class="normal small"><i>Note: Dragon mates (each pair) must be of the same species, maturity, and not closely-related.  They must also be adult and have full energy.</i></p>		
+			<p class="normal small">
+				<i
+					>Note: Dragon mates (each pair) must be of the same species, maturity, and not
+					closely-related. They must also be adult and have full energy.</i
+				>
+			</p>
 			<button class="btn btn-dark" on:click={() => breed()}>BREED</button>
 		</div>
-
 	</div>
 </div>
