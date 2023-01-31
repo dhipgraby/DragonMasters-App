486bff7d2942a502237b0adbb70a8f453112be60
Mark Suscens <mark.suscens@gmail.com>
2023-01-31 13:44:17 +0000

    Delete unused/redundant variables with hardcode values for offer details

--- src/lib/contracts/Marketplace.js
+++ src/lib/contracts/Marketplace.js
@@ -9,11 +9,6 @@ import { MarketApproval } from '$lib/contracts/MarketApproval';
 import { contracts } from "./contracts";
 import { get } from 'svelte/store';
 
-const salePriceInWei = '500000000000000000'  //0.5 ETH
-const rentPriceInWei = '10000000000000000'  // 0.01ETH
-const rentDepositInWei = '300000000000000000'  //0.3 ETH
-const rentMinTime = '2419200' //seconds: 2419200 == 4 weeks
-
 export const TokenType = { Unknown: 0, Dna: 1, Egg: 2, Dragon: 3 }
 export const OfferType = { NoOffer: 0, ForSale: 1, ForRent: 2, ForSaleOrRent: 3 }
 
