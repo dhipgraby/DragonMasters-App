9d1f4ff22b4c79513a5186f733e07519760beb22
kennow <kenneth.zambrano4@gmail.com>
2023-02-14 03:23:53 +0100

    Fix buttons for all offers

--- src/lib/component/marketplace/OfferBox.svelte
+++ src/lib/component/marketplace/OfferBox.svelte
@@ -5,12 +5,12 @@
 	export let rentTerms;
 	export let buy;
 	export let rent;
-	export let isForSale;	
+	export let isForSale;
 </script>
 
 {#if _offerType == OfferType.ForSale}
 	<button
-		class="btn btn-dark"
+		class="btn btn-dark singleButton"
 		on:click={async () => {
 			await buy();
 		}}
@@ -21,7 +21,7 @@
 
 {#if _offerType == OfferType.ForRent}
 	<button
-		class="btn btn-dark"
+		class="btn btn-dark singleButton"
 		on:click={async () => {
 			await rent();
 		}}
@@ -31,43 +31,67 @@
 {/if}
 
 {#if _offerType == OfferType.ForSaleOrRent}
-	{#if isForSale}
-		<button
-			class="btn btn-danger"
-			on:click={async () => {
-				await buy();
-			}}
-		>
-			Buy now <i class="fas fa-shopping-cart" />
-		</button>
-	{/if}
-	{#if rentTerms}
-		<button
-			class="btn btn-warning"
-			on:click={async () => {
-				await rent();
-			}}
-		>
-			Rent now <i class="fas fa-shopping-cart" />
-		</button>
-	{/if}
+	<div class="multiButton">
+		{#if isForSale}
+			<button
+				class="btn btn-dark"
+				on:click={async () => {
+					await buy();
+				}}
+			>
+				Buy now <i class="fas fa-shopping-cart" />
+			</button>
+		{/if}
+		{#if rentTerms}
+			<button
+				class="btn btn-warning"
+				on:click={async () => {
+					await rent();
+				}}
+			>
+				Rent now <i class="fas fa-shopping-cart" />
+			</button>
+		{/if}
+	</div>
 {/if}
 
 <style>
-	button {
-		position: absolute;
-		left: 0px;
-		bottom: -11px;
-		padding: 18px !important;
+
+	.multiButton {
 		width: 100%;
+		position: absolute;
+		display: inline-flex;
+		left: 0px;
+		bottom: 0px;
+		margin-top: 10px;
+	}
+
+	.multiButton button {
+		width: 100%;
+		padding: 10px !important;		
 		font-size: 18px;
-		border-bottom-right-radius: 17px;
-		border-bottom-left-radius: 17px;
 		border-top-right-radius: 0px;
 		border-top-left-radius: 0px;
 	}
 
-	button:hover {
+
+	.multiButton button:hover {		
+		opacity: 1;
+	}
+
+	.singleButton {
+		font-size: 18px;
+		padding: 10px !important;	
+		border-bottom-right-radius: 17px;
+		border-bottom-left-radius: 17px;
+		border-top-right-radius: 0px ;
+		border-top-left-radius: 0px;
+		position: absolute;
+		left: 0px;
+		bottom: 0px;
+	}
+
+	.singleButton:hover {
 		color: rgb(255, 181, 22);
 		opacity: 1;
 	}
